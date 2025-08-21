a6243433d6bcca7b989ea2fcea994eb58fa30660
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-08-21 10:19:00 +0630

    'network'

---
+++ network/client.py
@@ -0,0 +1,10 @@
+from socket import *
+serverName = 'localhost'
+serverPort = 12000
+clientSocket = socket(AF_INET,SOCK_DGRAM)
+message = input('input lowercase sentence: ')
+clientSocket.sendto(str.encode(message),
+                    (serverName,serverPort))
+modifiedMessage,serverAddress = clientSocket.recvfrom(2048)
+print(bytes.decode(modifiedMessage))
+clientSocket.close()
